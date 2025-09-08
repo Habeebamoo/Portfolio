ea4f7a91f8d64bfe5acbe877602a352c134b7708
Habeebamoo <habeebamoo08@gmail.com>
2025-09-08 14:30:26 +0100

    added animation

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,20 +7,20 @@ import { SlArrowRight } from "react-icons/sl"
 const Main = () => {
   return (
     <main className="bg-primary p-2">
-      <div className="h-30 w-30 rounded-full overflow-hidden mt-30 mx-auto">
+      <div className="h-30 w-30 rounded-full overflow-hidden mt-30 mx-auto" data-aos="slide-down">
         <img src={anime} className="h-full w-full object-cover" />
       </div>
-      <div className="mt-4">
+      <div className="mt-4" data-aos="zoom-in">
         <h1 className="text-accent font-roboto text-3xl text-center">Full-Stack <span className="text-secondary">Developer</span></h1>
       </div>
-      <p className="text-muted text-sm mt-2 text-center">While crafting premium digital experiences with modern technologies. I enjoy creating fast, secure and reliable solutions</p>
-      <div className="mt-3 flex-center gap-3">
+      <p className="text-muted text-sm mt-2 text-center" data-aos="zoom-in">While crafting premium digital experiences with modern technologies. I enjoy creating fast, secure and reliable solutions</p>
+      <div className="mt-3 flex-center gap-3" data-aos="zoom-in">
         <button className="btn-secondary text-[12px] font-outfit">View My Work</button>
         <button className="btn-primary text-[12px] font-outfit">Download Resume</button>
       </div>
 
       <div className="mt-10">
-        <div className="py-8 px-4 bg-box border-1 border-border rounded-md w-[90%] md:w-[500px] mx-auto">
+        <div className="py-8 px-4 bg-box border-1 border-border rounded-md w-[90%] md:w-[500px] mx-auto" data-aos="slide-up">
           <h1 className="text-accent font-outfit text-lg">About Me</h1>
           <p className="text-sm text-muted font-inter mt-4">I'm Habeeb Amoo, a 17 year old software developer from Nigeria who enjoys turning ideas into working applications. With 2+ years hands on experience, i like building end-to-end products from designing user expeiences to creating reliable systems behind the scenes.</p>
           <p className="text-sm text-muted font-inter mt-3">I'm always learning, experimenting and improving the way i approach problems</p>
@@ -31,9 +31,9 @@ const Main = () => {
         <FaArrowDown size={19} />
       </div>
 
-      <h1 className="text-accent text-center font-inter text-lg">What I've Been Up To</h1>
-      <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-5 w-[90%] md:w-[800px]  mx-auto">
-        <div className="p-4 bg-box border-1 border-border rounded-md">
+      <h1 className="text-accent text-center font-inter text-xl">What I've Been Up To</h1>
+      <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-5 w-[90%] md:w-[800px] mx-auto">
+        <div className="p-4 bg-box border-1 border-border rounded-md" data-aos="zoom-in">
           <div className="rounded-md overflow-hidden">
             <img src={maildrop1} />
           </div>
@@ -47,7 +47,7 @@ const Main = () => {
           </button>
         </div>
 
-        <div className="p-4 bg-box border-1 border-border rounded-md">
+        <div className="p-4 bg-box border-1 border-border rounded-md" data-aos="zoom-in">
           <div className="rounded-md overflow-hidden">
             <img src={flashquiz1} />
           </div>
