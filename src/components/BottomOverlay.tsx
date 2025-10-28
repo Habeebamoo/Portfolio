bc0b31ee3cd6963b07b65c1592e18f1e54fa3abb
Habeebamoo <habeebamoo08@gmail.com>
2025-10-28 22:31:49 +0100

    created technologies section

---
+++ src/components/BottomOverlay.tsx
@@ -0,0 +1,9 @@
+const BottomOverlay = () => {
+  return (
+    <div className="fixed left-0 bottom-[-60px] right-0 h-10 backdrop-blur-md bg-white/30">
+
+    </div>
+  )
+}
+
+export default BottomOverlay
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,13 @@
 import ProjectDisplay from "./ProjectDisplay"
 import maildropImg from "../assets/maildrop-img1.png"
 import flashquizImg from "../assets/flash-img1.png"
+import { FaGolang } from "react-icons/fa6"
+import { DiJavascript, DiNodejs, DiPostgresql } from "react-icons/di"
+import { MdJavascript } from "react-icons/md"
+import { BiLogoJavascript, BiLogoPostgresql } from "react-icons/bi"
+import { SiPostgresql, SiTypescript } from "react-icons/si"
+import { FaNodeJs, FaReact } from "react-icons/fa"
+import { LiaNodeJs } from "react-icons/lia"
 
 const Main = () => {
   return (
@@ -20,7 +27,7 @@ const Main = () => {
       </section>
 
       {/* Projects */}
-      <section className="mt-30 mb-30 w-[90%] md:w-[800px] mx-auto">
+      <section className="mt-30 w-[90%] md:w-[800px] mx-auto">
         <h1 className="font-inter font-bold text-xl text-center mb-10">What I've Been Up To.</h1>
 
         {/* display */}
@@ -37,6 +44,27 @@ const Main = () => {
           />
         </div>
       </section>
+
+      {/* Tech Stack */}
+      <section className="mt-30 mb-30 w-[90%] md:w-[400px] mx-auto">
+        <h1 className="font-inter font-bold text-xl text-center mb-7">Technologies.</h1>   
+
+        <div className="relative overflow-hidden w-[300px] rounded-3xl mx-auto p-5">
+          {/* gradient edges */}
+          <div className="pointer-events-none absolute inset-0 z-10 bg-gradient-to-r from-gray-50 via-transparent to-gray-50"></div>
+
+          {/* content */}
+          <div className="flex-center gap-3 absolute top-0 bottom-0 animate-scroll">
+            <div className="text-blue-400"><FaGolang size={50} /></div>
+            <div className="text-yellow-300"><BiLogoJavascript size={40} /></div>
+            <div className="text-blue-800"><SiTypescript size={30} /></div>
+            <div className="text-blue-400"><FaReact size={30} /></div>
+            <div className="text-green-500"><LiaNodeJs size={40} /></div>
+            <div className="text-cyan-700"><BiLogoPostgresql size={35} /></div>
+          </div>
+        </div>
+      </section>
+
     </>
   )
 }
