2477bd80e6a09164482063a3f21a8a64859125eb
Habeebamoo <habeebamoo08@gmail.com>
2025-10-29 16:43:18 +0100

    Updated icon colors in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
 
   return (
     <header className="fixed z-10 top-0 left-0 right-0 p-10 flex-center">
-      <nav className="flex-center shadow-2xl gap-6 bg-white py-3 px-6 rounded-lg border-1 border-mutedLg">
+      <nav className="flex-center shadow-2xl gap-6 bg-white py-3 px-6 rounded-lg border-1 border-mutedLg text-muted">
         <div onClick={toGitHub}>
           <BsGithub size={26} />
         </div>
