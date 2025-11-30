53944e3fb1f39fdf6ba7246dc7314baf67c501c0
Habeebamoo <habeebamoo08@gmail.com>
2025-11-30 19:26:53 +0100

    updated Project display

--- src/components/ProjectDisplay.tsx
+++ src/components/ProjectDisplay.tsx
@@ -19,7 +19,7 @@ const ProjectDisplay = ({ img, title, description, location }: Props) => {
       <img src={img} className="rounded-md border-1 border-mutedLg shadow-xl" />
       <div className="pl-1 mb-2 mt-6">
         <h1 className="font-inter font-bold text-xl">{title}</h1>
-        <p className="mt-2 font-inter text-gray-600 mb-4">{description}</p>
+        <p className="mt-2 font-inter text-[15px] text-gray-600 mb-4">{description}</p>
         <button onClick={toProject} className="py-3 px-5 bg-black border-1 cursor-pointer border-black text-white text-sm font-jsans rounded-lg active:bg-transparent hover:bg-transparent hover:text-black active:text-black">View Project</button>
       </div>
     </div>
