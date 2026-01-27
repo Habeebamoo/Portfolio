7e831ab6eb9207491f785885d755cd284985cb8c
Habeebamoo <habeebamoo08@gmail.com>
2026-01-27 18:08:13 +0100

    used <a /> tags for profile links in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,34 @@
 import { BsGithub, BsLinkedin, BsTwitterX } from "react-icons/bs"
 
 const Header = () => {
-  const toGitHub = () => {
-    window.location.href = "https://github.com/Habeebamoo"
-  }
-
-  const toLinkedIn = () => {
-    window.location.href = "https://linkedin.com/in/habeeb-amoo-2a5787380"
-  }
-
-  const toTwitter = () => {
-    window.location.href = "https://x.com/Habeeb_O_Amoo"
-  }
-
   return (
     <header className="fixed z-20 top-0 left-0 right-0 p-8 flex-center">
       <nav className="flex-center gap-6 glass-header border border-gray-100 rounded-2xl py-4 px-6 text-[#444]">
-        <div onClick={toGitHub}>
+
+        <a 
+          href="https://github.com/Habeebamoo"
+          target="_blank"
+          rel="noopener noeferrer"
+        >
           <BsGithub size={26} />
-        </div>
-        <div onClick={toTwitter}>
+        </a>
+
+        <a 
+          href="https://x.com/Habeeb_O_Amoo"
+          target="_blank"
+          rel="noopener noeferrer"
+        >
           <BsTwitterX size={25} />
-        </div>
-        <div onClick={toLinkedIn}>
+        </a>
+
+        <a 
+          href="https://linkedin.com/in/habeeb-amoo-2a5787380"
+          target="_blank"
+          rel="noopener noeferrer"
+        >
           <BsLinkedin size={25} />
-        </div>
+        </a>
+
       </nav>
     </header>
   )
